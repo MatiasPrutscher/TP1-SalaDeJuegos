70bdc0ad6a889126327e29a034a77b6564f5737c
Matias Prutscher <matiasprutscher@gmail.com>
2025-04-24 19:43:16 -0300

    Optimización de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,10 @@
 import { Routes, RouterModule } from '@angular/router';
-import { NgModule } from '@angular/core';
 import { HomeComponent } from './pages/home/home.component';
-import { LoginComponent } from './pages/login/login.component';
-import { RegistroComponent } from './pages/registro/registro.component';
-import { QuienSoyComponent } from './pages/quien-soy/quien-soy.component';
+
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
-    {path: 'login', component: LoginComponent},
-    {path: 'registro', component: RegistroComponent},
-    {path: 'quien-soy', component: QuienSoyComponent}
+    {path: 'login', loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent)},
+    {path: 'registro', loadComponent: () => import('./pages/registro/registro.component').then(m => m.RegistroComponent)},
+    {path: 'quien-soy', loadComponent: () => import('./pages/quien-soy/quien-soy.component').then(m => m.QuienSoyComponent)},
 ];
-@NgModule({
-    imports: [RouterModule.forRoot(routes, { onSameUrlNavigation: 'reload' })],
-    exports: [RouterModule],
-  })
-  export class AppRoutingModule {}
